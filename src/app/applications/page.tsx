3b0cafd18a5ea79e3fef77ee556e36a962e4d517
ivanotch <babidaivan09@gmail.com>
2024-04-02 02:46:38 +0800

    Added Nav Bar Design, Initial commit

---
+++ src/app/applications/page.tsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+export default function Application() {
+    return (
+        <main className="flex flex-col items-center justify-between p-24">
+            Application page
+        </main>
+    );
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,27 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import { UserButton } from "@clerk/nextjs";
+
+export default function Nav () {
+    return (
+        <nav className='flex h-[3.5rem] justify-between items-center px-[2rem] border-[#2d2d2d] border-b'>
+            <span className='text-[1.1em] font-semibold'>
+                <span className='text-[1.6em] font-semibold bg-gradient-to-r from-indigo-500 to-[#95A1F9] text-transparent bg-clip-text'>EARIST </span> 
+                <span className='text-[1.6em] font-semibold bg-gradient-to-r from-indigo-500 to-[#95A1F9] text-transparent bg-clip-text'> O</span>rg 
+                <span className='text-[1.6em] font-semibold bg-gradient-to-r from-indigo-500 to-[#95A1F9] text-transparent bg-clip-text'> S</span>aya
+            </span>
+            <ul className='flex text-[0.9em]'>
+                <li className='mr-[10px]'><Link href={"/"}>Dashboard </Link></li>
+                /
+                <li className='ml-[10px]'><Link href={"/applications"}> Application</Link></li>
+            </ul>
+            <ul className='flex w-[7rem] justify-around'>
+                <li>
+                    <img className='' src="/notification.svg" alt="Notification" width={35} height={35}/>
+                </li>
+                <li><UserButton /></li>
+            </ul>
+        </nav>
+    )
+}
